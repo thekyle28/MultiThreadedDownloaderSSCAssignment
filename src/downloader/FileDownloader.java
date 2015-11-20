aa5f5274b373b98ac6f7115312b268f15e78ea0b
Kyle <karat28@hotmail.co.uk>
2015-11-20 01:07:59 +0000

    Multithreading now works properly

--- src/downloader/FileDownloader.java
+++ src/downloader/FileDownloader.java
@@ -35,79 +35,99 @@ public class FileDownloader {
 	}
 
 	class Download implements Runnable {
-		
-		public void run() {
-			Document doc;
-			try {
-				doc = Jsoup.connect(url).get();
-				System.out.println(Thread.currentThread().getName().toString() + ": " + Thread.currentThread().getState().toString());
-				Elements imgs = doc.select("img[src~=(?i)\\.(" + filter + ")]");
-				// get all links
-				// Note: a hyper link object is defined using <a> tag, the the
-				// link is
-				// defined using attribute [href]
-				// We use selector to search elements a hyper link object with
-				// attribute
-				// [href],
-				Elements links = doc.select("a[href$=\"" + filter + "\"]");
 
-				for (Element link : links) {
-					System.out.println(Thread.currentThread().getName().toString() + ": " + Thread.currentThread().getState().toString());
-					// get the value from href attribute: link.attr("href")
-					System.out.println("\nlink : " + link.attr("href"));
-					System.out.println("text : " + link.text());
+		private Element file;
+		private String urlstr;
+		private String fileName;
 
-				}
-
-				for (Element img : imgs) {
-					System.out.println(Thread.currentThread().getName().toString() + ": " + Thread.currentThread().getState().toString());
-					String urlstr = img.attr("src");
-					System.out.println(urlstr);
-					if (urlstr.indexOf(url) <= 0)
-						urlstr = url + urlstr;
-					System.out.println(urlstr);
-
-					String fileName = urlstr.substring(
-							urlstr.lastIndexOf('/') + 1, urlstr.length());
-					System.out.println(fileName);
-
-					// Open a URL Stream
-					URL url;
-					try {
-						System.out.println(Thread.currentThread().getName().toString() + ": " + Thread.currentThread().getState().toString());
-
-						url = new URL(urlstr);
-
-						InputStream in = url.openStream();
-						OutputStream out = new BufferedOutputStream(
-								new FileOutputStream(saveLoc + fileName));
-						System.out.println(saveLoc + fileName);
-						for (int b; (b = in.read()) != -1;) {
-							out.write(b);
-						}
-						out.close();
-						in.close();
-						System.out.println(Thread.currentThread().getName().toString() + ": " + Thread.currentThread().getState().toString());
-
-					} catch (IOException e1) {
-						e1.printStackTrace();
-
-					}
-					System.out.println(img.attr("src"));
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		public Download(Element file, String urlstr, String fileName) {
+			this.file = file;
+			this.urlstr = urlstr;
+			this.fileName = fileName;
 		}
 
+		public void run() {
+
+			// Open a URL Stream
+			URL url;
+			try {
+				System.out.println(Thread.currentThread().getName().toString()
+						+ ": " + Thread.currentThread().getState().toString());
+
+				url = new URL(urlstr);
+
+				InputStream in = url.openStream();
+				OutputStream out = new BufferedOutputStream(
+						new FileOutputStream(saveLoc + fileName));
+				System.out.println(saveLoc + fileName);
+				for (int b; (b = in.read()) != -1;) {
+					out.write(b);
+				}
+				out.close();
+				in.close();
+				System.out.println(Thread.currentThread().getName().toString()
+						+ ": " + Thread.currentThread().getState().toString());
+
+			} catch (IOException e1) {
+				e1.printStackTrace();
+
+			}
+		}
 	}
 
 	public void download() throws IOException {
 
-		// Step 1: create an a fix thread pool of size that the user specified.
-		ExecutorService pool = Executors.newFixedThreadPool(numThreads);
-		Download download = new Download();
-		pool.submit(download);
+		Document doc;
+		try {
+			//connect to the webpage
+			doc = Jsoup.connect(url).get();
+			//grab all of the images on the webpage with the file extension the user defined.
+			Elements imgs = doc.select("img[src~=(?i)\\.(" + filter + ")]");
+			// get all links
+			// Note: a hyper link object is defined using <a> tag, the the
+			// link is
+			// defined using attribute [href]
+			// We use selector to search elements a hyper link object with
+			// attribute
+			// [href],
+			Elements links = doc.select("a[href$=\"" + filter + "\"]");
+			
 
+			ExecutorService pool = Executors.newFixedThreadPool(numThreads);
+
+			//for each link 
+			for (Element link : links) {
+				System.out.println(Thread.currentThread().getName().toString()
+						+ ": " + Thread.currentThread().getState().toString());
+				// get the value from href attribute: link.attr("href")
+				System.out.println("\nlink : " + link.attr("href"));
+				System.out.println("text : " + link.text());
+
+			}
+			
+			for (Element img : imgs) {
+				System.out.println(Thread.currentThread().getName().toString()
+						+ ": " + Thread.currentThread().getState().toString());
+				String urlstr = img.attr("src");
+				System.out.println(urlstr);
+				if (urlstr.indexOf(url) <= 0)
+					urlstr = url + urlstr;
+				System.out.println(urlstr);
+
+				String fileName = urlstr.substring(urlstr.lastIndexOf('/') + 1,
+						urlstr.length());
+
+				// Step 1: create an a fix thread pool of size that the user
+				// specified.
+				
+				Download download = new Download(img, urlstr, fileName);
+				pool.submit(download);
+
+
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
